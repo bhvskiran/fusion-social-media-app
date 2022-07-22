ab7d702c1d93f7e5c57629fa6e0c31038829e66e
bhvskiran <bhvskiran@gmail.com>
2022-07-22 23:59:25 +0530

    day 5 - completed right side as well as home page

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Profile.css"
+
+const Profile = () => {
+  return (
+    <div>
+        Profile
+        {/* 1 39 13 */}
+    </div>
+  )
+}
+
+export default Profile
