fd8a28b9e5624bfe05e92a93fdc9521ff32077f7
bhvskiran <bhvskiran@gmail.com>
2022-07-28 23:43:05 +0530

    added the app fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
-import Auth from "./pages/Auth/Auth";
-import Home from "./pages/Home/Home";
+// import Auth from "./pages/Auth/Auth";
+// import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 
 function App() {
